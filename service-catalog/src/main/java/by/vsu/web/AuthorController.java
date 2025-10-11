2e88a05c00f3b40ab7188c29516bc65920ade528
Yermochenko Sergey <yermochenko@gmail.com>
2025-10-11 22:28:52 +0300

    Расширен endpoint /api/author
    Добавлено получение автора по идентификатору

--- service-catalog/src/main/java/by/vsu/web/AuthorController.java
+++ service-catalog/src/main/java/by/vsu/web/AuthorController.java
@@ -32,14 +32,40 @@ public class AuthorController extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		ObjectMapper mapper = new ObjectMapper().registerModule(new Jdk8Module());
 		try {
-			List<Author> authors = authorService.findAll();
-			ObjectMapper mapper = new ObjectMapper().registerModule(new Jdk8Module());
-			resp.setStatus(HttpServletResponse.SC_OK);
-			resp.setContentType("application/json");
-			mapper.writeValue(resp.getOutputStream(), authors);
+			String idParam = req.getParameter("id");
+			if(idParam != null) {
+				try {
+					Author author = authorService.findById(Long.parseLong(idParam)).orElseThrow(IllegalArgumentException::new);
+					resp.setStatus(HttpServletResponse.SC_OK);
+					resp.setContentType("application/json");
+					mapper.writeValue(resp.getOutputStream(), author);
+				} catch(IllegalArgumentException e) {
+					resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+					resp.setContentType("application/json");
+					mapper.writeValue(resp.getOutputStream(), new Error("Nothing found"));
+				}
+			} else {
+				List<Author> authors = authorService.findAll();
+				resp.setStatus(HttpServletResponse.SC_OK);
+				resp.setContentType("application/json");
+				mapper.writeValue(resp.getOutputStream(), authors);
+			}
 		} catch(ServiceException e) {
 			throw new ServletException(e);
+		}
+	}
+
+	public static class Error {
+		private final String message;
+
+		public Error(String message) {
+			this.message = message;
+		}
+
+		public String getMessage() {
+			return message;
 		}
 	}
 }
